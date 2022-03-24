3686b163d83a0cd6e09fbd2ab994834775d52d80
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-03-24 14:55:48 -0700

    finished pseduocode

--- test_1.py
+++ test_1.py
@@ -14,3 +14,30 @@ If the amount is equal, we ouput to the user "Option 1 and Option 2 pays the sam
 If the option1 is better, we output to the user "Option 1 is better"
 If the option2 is better, we output to the user "Option 2 is better"
 """
+
+"""
+# option1
+return 100 * 10
+
+# option2
+amount = 1 
+list1 = []
+loop 10 times
+    add amount to list1
+    amount *= 2
+sum = sum of all items in loop
+return sum 
+
+# main 
+    var 1 = option1 
+    var 2 = option2 
+
+if var1 = var2
+    "Option 1 and Option 2 pays the same"
+if var1 < var2
+    "Option 2 is better"
+else
+    "Option 1 is better"
+
+main
+"""
