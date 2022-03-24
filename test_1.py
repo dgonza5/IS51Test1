d4a570c5cddf40f935429342b9fe384559abc59c
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-03-24 14:45:08 -0700

    finsihed structed english

---
+++ test_1.py
@@ -0,0 +1,16 @@
+"""
+The program is trying to determine which payment option is better (more money). 
+First option is 100 dollars per day for 10 days.
+Second option is 1 dollar a day with it being doubled each day for 10 days. 
+There will be two functions to calculate the pay rate. 
+
+Function-1 will calculate the amount for the first option. 
+Function-2 will calculate the amount for the second option. 
+
+function1 will output 100 * 10 days.
+function2 will loop 10 times, with each time, doubling the amount and add the amount to the total. 
+
+If the amount is equal, we ouput to the user "Option 1 and Option 2 pays the same". 
+If the option1 is better, we output to the user "Option 1 is better"
+If the option2 is better, we output to the user "Option 2 is better"
+"""
